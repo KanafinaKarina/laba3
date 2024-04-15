dc230c0496f50d599202eeb8f8fe339ef9c52abf
KanafinaKarina <145910889+KanafinaKarina@users.noreply.github.com>
2024-04-15 23:46:55 +0500

    Update Cylinder.h

--- laba3/Cylinder.h
+++ laba3/Cylinder.h
@@ -1,14 +1,38 @@
+#ifndef CYLINDER_H
+#define CYLINDER_H
+#include <iostream>
+#include <string>
 #include "Shape3D.h"
 #include "Circle.h"
 
+
 class Cylinder : public Shape3D {
 private:
-    float height;
-    Circle base3D;
+	float height;
+	Circle baseCircle;
 public:
-    Cylinder(float height_, Circle base3D_);
-    void CalculateVolume() override;
-    void Scale(float ScaleFactor) override;
-    void ShowInfo() override;
-    std::string GetName() override;
+	Cylinder(float h, float r) : height(h), baseCircle(r) {} // Constructor
+	void CalculateVolume() override {
+		baseCircle.CalculateArea();
+		volume = baseCircle.GetArea() * height;
+	}
+
+	float GetVolume() override {
+		return volume;
+	}
+	void Scale(float scaleFactor) override {
+		volume = volume * scaleFactor;
+	}
+	void ShowInfo() override {
+		CalculateVolume();
+		std::cout << GetName() << std::endl;
+		std::cout << "Cylinder with height:" << height << " and volume: " << GetVolume() << std::endl << std::endl;
+	}
+	std::string GetName() override {
+		return "Cylinder";
+	}
+	bool operator>(Shape3D& other) override { return volume > other.GetVolume(); }
+	bool operator<(Shape3D& other) override { return volume < other.GetVolume(); }
+	bool operator==(Shape3D& other) override { return volume == other.GetVolume(); }
 };
+#endif CYLINDER_H
