257b616a93b7c5cb0600732d7f6ba59235229af2
KanafinaKarina <145910889+KanafinaKarina@users.noreply.github.com>
2024-04-15 23:47:30 +0500

    Update Shape.h

--- laba3/Shape.h
+++ laba3/Shape.h
@@ -1,15 +1,12 @@
-#include <string>
-
 #ifndef SHAPE_H
 #define SHAPE_H
+#include <string>
+
 
 class Shape {
-private:
-
 public:
-    virtual void Scale(float ScaleFactor) = 0;
-    virtual void ShowInfo() = 0;
-    virtual std::string GetName() = 0;
+	virtual void Scale(float scaleFactor) = 0;
+	virtual void ShowInfo() = 0;
+	virtual std::string GetName() = 0;
 };
-
-#endif
+#endif SHAPE_H
