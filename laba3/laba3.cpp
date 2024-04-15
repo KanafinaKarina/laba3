1920ff0c0728e68c21c31e8ba5d3534fa491f0b3
KanafinaKarina <145910889+KanafinaKarina@users.noreply.github.com>
2024-04-15 23:44:52 +0500

    Update laba3.cpp

--- laba3/laba3.cpp
+++ laba3/laba3.cpp
@@ -1,77 +1,41 @@
-﻿#include "Square.h"
+﻿#include <iostream>
+#include <string>
+#include "Shape.h"
+#include "Shape2D.h"
+#include "Shape3D.h"
+#include "Square.h"
 #include "Triangle.h"
 #include "Circle.h"
-#include <iostream>
 #include "TriangularPyramid.h"
 #include "Cylinder.h"
 #include "Sphere.h"
 
-void test_2d(Square& s, Triangle& t, Circle& c) {
-
-    s.ShowInfo();
-    s.CalculateArea();
-    s.ShowInfo();
-    s.Scale(2);
-    s.ShowInfo();
-
-    t.CalculateArea();
-    t.ShowInfo();
-
-    c.CalculateArea();
-    c.ShowInfo();
-    if (t > s)std::cout << "t>s" << std::endl;
-    else if (t < s)std::cout << "t<s" << std::endl;
-    else std::cout << "t==s" << std::endl;
-    t.Scale(20);
-    t.ShowInfo();
-    if (t > s)std::cout << "t>s" << std::endl;
-    else if (t < s)std::cout << "t<s" << std::endl;
-    else std::cout << "t==s" << std::endl;
-}
-
-void test_3d(TriangularPyramid& tp, Cylinder& cyl, Sphere& s) {
-    tp.ShowInfo();
-    tp.CalculateVolume();
-    tp.ShowInfo();
-    tp.Scale(2);
-    tp.ShowInfo();
-
-    cyl.ShowInfo();
-    cyl.CalculateVolume();
-    cyl.ShowInfo();
-    cyl.Scale(3);
-    cyl.ShowInfo();
-
-    if (tp > cyl)std::cout << "tp>cyl" << std::endl;
-    else if (tp < cyl)std::cout << "tp<cyl" << std::endl;
-    else std::cout << "tp==cyl" << std::endl;
-
-    s.ShowInfo();
-    s.CalculateVolume();
-    s.ShowInfo();
-    s.Scale(4);
-    s.ShowInfo();
-
-    if (tp > s)std::cout << "tp>s" << std::endl;
-    else if (tp < s)std::cout << "tp<s" << std::endl;
-    else std::cout << "tp==s" << std::endl;
-}
-
-
-
 int main() {
-    // Shape2D ;
-    Square sq1(10);
-    Triangle tr1(10, 2);
-    Circle c1(5);
+	Square sq(3.0);
+	sq.CalculateArea();
+	sq.ShowInfo();
 
-    test_2d(sq1, tr1, c1);
+	Triangle tr(2, 5);
+	tr.CalculateArea();
+	tr.ShowInfo();
 
-    // Shape3D 
-    TriangularPyramid trp1(5, tr1);
-    Cylinder cyl1(6, c1);
-    Sphere s1(10);
+	Circle cr(5);
+	cr.CalculateArea();
+	cr.ShowInfo();
 
-    test_3d(trp1, cyl1, s1);
+	TriangularPyramid pyramid(6, 5, 4);
+	pyramid.CalculateVolume();
+	pyramid.ShowInfo();
 
+	Cylinder cyl(7, 2);
+	cyl.CalculateVolume();
+	cyl.ShowInfo();
+
+
+	Sphere sp(4);
+	sp.CalculateVolume();
+	sp.ShowInfo();
+
+
+	return 0;
 }
