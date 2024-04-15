f3bf61127bf6d4faa450b159ad6c95eb3dd30639
KanafinaKarina <145910889+KanafinaKarina@users.noreply.github.com>
2024-04-15 23:49:32 +0500

    Update Sphere.h

--- laba3/Sphere.h
+++ laba3/Sphere.h
@@ -1,12 +1,38 @@
+#ifndef SPHERE_H
+#define SPHERE_H
+#include <iostream>
+#include <string>
 #include "Shape3D.h"
 
 class Sphere : public Shape3D {
 private:
-    float radius;
+	float radius;
 public:
-    Sphere(float radius_);
-    void CalculateVolume() override;
-    void Scale(float ScaleFactor) override;
-    void ShowInfo() override;
-    std::string GetName() override;
+	Sphere(float r) : radius(r) {} // Constructor
+
+	float GetVolume() override {
+		return volume;
+	}
+	void Scale(float scaleFactor) override {
+		volume = volume * scaleFactor;
+	}
+	void ShowInfo() override {
+		std::cout << GetName() << std::endl;
+		std::cout << "Sphere with radius: " << radius << " and volume: " << GetVolume() << std::endl;
+	}
+	std::string GetName() override {
+		return "Sphere";
+	}
+	bool operator>(Shape3D& other) override {
+		return volume > other.GetVolume();
+	}
+	bool operator<(Shape3D& other) override {
+		return volume < other.GetVolume();
+	}
+	bool operator==(Shape3D& other) override { return volume == other.GetVolume(); }
+	void CalculateVolume() override {
+		volume = (4.0 / 3.0) * 3.141592 * radius * radius * radius;
+	}
 };
+
+#endif SPHERE_H
